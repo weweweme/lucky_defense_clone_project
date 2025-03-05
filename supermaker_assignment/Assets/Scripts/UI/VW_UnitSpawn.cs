718dbc330b8762393d1d12ac94e3132324d01a16
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-05 23:59:04 +0900

    feat: VW_UnitSpawn 클래스 구현

---
+++ supermaker_assignment/Assets/Scripts/UI/VW_UnitSpawn.cs
@@ -0,0 +1,22 @@
+using InGame.System;
+using UnityEngine;
+using UnityEngine.UI;
+using Util;
+
+namespace UI
+{
+    /// <summary>
+    /// Unit Spawn UI의 View 클래스입니다.
+    /// </summary>
+    public sealed class VW_UnitSpawn : View
+    {
+        [SerializeField] internal Button btnSpawn;
+        [SerializeField] internal Text currentCurrency; 
+        
+        private void Awake()
+        {
+            AssertHelper.NotNull(typeof(VW_UnitSpawn), btnSpawn);
+            AssertHelper.NotNull(typeof(VW_UnitSpawn), currentCurrency);
+        }
+    }
+}
