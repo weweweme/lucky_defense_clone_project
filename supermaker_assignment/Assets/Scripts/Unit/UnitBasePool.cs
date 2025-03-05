8e19f12b3cf52109616b751926577a739ea0b062
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-06 01:17:30 +0900

    feat: UnitBasePool 클래스 스크립트 추가

---
+++ supermaker_assignment/Assets/Scripts/Unit/UnitBasePool.cs
@@ -0,0 +1,11 @@
+using Util;
+
+namespace Unit
+{
+    /// <summary>
+    /// 유닛 오브젝트 베이스를 관리하는 풀 클래스입니다.
+    /// </summary>
+    public class UnitBasePool : ObjectPoolBase<UnitRoot>
+    {
+    }
+}
