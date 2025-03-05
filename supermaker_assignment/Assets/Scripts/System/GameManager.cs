a69534abc148244a84510d1b41cf4416b066b737
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-05 13:26:10 +0900

    feat: GameManager 스크립트 추가

---
+++ supermaker_assignment/Assets/Scripts/System/GameManager.cs
@@ -0,0 +1,11 @@
+using Util;
+
+namespace System
+{
+    /// <summary>
+    /// 개임 내에서 사용될 매니저들의 참조와 초기화를 담당합니다.
+    /// </summary>
+    public sealed class GameManager : Singleton<GameManager>
+    {
+    }
+}
