a9702d64d51e0907e05758c28230394241e4a07e
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-06 21:57:19 +0900

    feat: PR_UserSpawnInfo 클래스 구현

---
+++ supermaker_assignment/Assets/Scripts/UI/PR_UserSpawnInfo.cs
@@ -0,0 +1,24 @@
+using System;
+using InGame.System;
+using Model;
+using Util;
+
+namespace UI
+{
+    /// <summary>
+    /// User Spawn Info와 관련된 UI의 비즈니스 로직을 담당하는 Presenter 클래스입니다.
+    /// </summary>
+    public class PR_UserSpawnInfo : Presenter
+    {
+        public override void Init(DataManager dataManager, View view)
+        {
+            AssertHelper.NotNull(typeof(PR_UnitSpawn), dataManager);
+            
+            MDL_Unit mdl = dataManager.Unit;
+            AssertHelper.NotNull(typeof(PR_UnitSpawn), mdl);
+            
+            VW_UnitSpawn vwUnitSpawn = view as VW_UnitSpawn;
+            AssertHelper.NotNull(typeof(PR_UnitSpawn), vwUnitSpawn);
+        }
+    }
+}
