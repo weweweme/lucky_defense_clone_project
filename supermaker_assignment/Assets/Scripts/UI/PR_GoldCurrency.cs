27c8f4be8f50a959b4a19da4b508c9d2694e29d4
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-06 20:26:18 +0900

    refactor: 클래스 네이밍 변경

--- supermaker_assignment/Assets/Scripts/UI/PR_GoldCurrency.cs
+++ supermaker_assignment/Assets/Scripts/UI/PR_GoldCurrency.cs
@@ -9,14 +9,14 @@ namespace UI
     /// <summary>
     /// 재화와 관련된 UI를 관리하는 Presenter 클래스입니다.
     /// </summary>
-    public class PR_Currency : Presenter
+    public class PR_GoldCurrency : Presenter
     {
         public override void Init(DataManager dataManager, View view)
         {
-            AssertHelper.NotNull(typeof(PR_Currency), dataManager);
+            AssertHelper.NotNull(typeof(PR_GoldCurrency), dataManager);
 
             MDL_Currency mdl = dataManager.Currency;
-            AssertHelper.NotNull(typeof(PR_Currency), mdl);
+            AssertHelper.NotNull(typeof(PR_GoldCurrency), mdl);
             
             VW_Currency vw = view as VW_Currency;
             AssertHelper.NotNull(typeof(PR_UnitSpawn), vw);
