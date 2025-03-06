874f6497a8d8db1265f9f90fb56f9a700d5c6d15
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-07 02:14:59 +0900

    feat: SUnitPlacementDragData 구조체 구현

--- supermaker_assignment/Assets/Scripts/System/Structures.cs
+++ supermaker_assignment/Assets/Scripts/System/Structures.cs
@@ -58,4 +58,31 @@ namespace System
             SpawnSide = spawnSide;
         }
     }
+    
+    /// <summary>
+    /// 유닛 배치 노드의 드래그 상태와 타겟 노드 정보를 담는 구조체입니다.
+    /// </summary>
+    public readonly struct SUnitPlacementDragData
+    {
+        /// <summary>
+        /// 현재 드래그 상태 여부입니다.
+        /// </summary>
+        public bool IsDragging { get; }
+
+        /// <summary>
+        /// 드래그 중 마우스 위치에 가장 가까운 유닛 배치 노드입니다.
+        /// </summary>
+        public UnitPlacementNode TargetNode { get; }
+
+        /// <summary>
+        /// 유닛 배치 드래그 데이터를 생성하는 생성자입니다.
+        /// </summary>
+        /// <param name="isDragging">드래그 상태 여부</param>
+        /// <param name="targetNode">드래그 타겟 노드</param>
+        public SUnitPlacementDragData(bool isDragging, UnitPlacementNode targetNode)
+        {
+            IsDragging = isDragging;
+            TargetNode = targetNode;
+        }
+    }
 }
