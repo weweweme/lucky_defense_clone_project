d3c452682c3bb6f1a27e2eeabe8e46635fb8956c
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-06 20:44:20 +0900

    feat: 유닛 스폰에 필요한 골드량을 표기하는 PR_SpawnNeededGold 클래스 구현

---
+++ supermaker_assignment/Assets/Scripts/UI/PR_SpawnNeededGold.cs
@@ -0,0 +1,27 @@
+using System;
+using InGame.System;
+using Model;
+using Util;
+using UniRx;
+
+namespace UI
+{
+    /// <summary>
+    /// 유닛 스폰에 필요한 골드량을 표기하는 Presenter 클래스입니다.
+    /// </summary>
+    public class PR_SpawnNeededGold : Presenter
+    {
+        public override void Init(DataManager dataManager, View view)
+        {
+            VW_Currency vwCurrency = view as VW_Currency;
+            AssertHelper.NotNull(typeof(PR_SpawnNeededGold), vwCurrency);
+            
+            MDL_Unit mdl = dataManager.Unit;
+            AssertHelper.NotNull(typeof(PR_SpawnNeededGold), mdl);
+            
+            mdl.SpawnNeededGold
+                .Subscribe(vwCurrency!.UpdateCurrency)
+                .AddTo(disposable);
+        }
+    }
+}
