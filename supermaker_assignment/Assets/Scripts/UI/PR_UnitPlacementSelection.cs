1101de0ed40334ee486284eb95d28ac4ee0dd0c9
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-07 03:25:58 +0900

    feat: PR_UnitPlacementSelection 내부 Rx 연동

--- supermaker_assignment/Assets/Scripts/UI/PR_UnitPlacementSelection.cs
+++ supermaker_assignment/Assets/Scripts/UI/PR_UnitPlacementSelection.cs
@@ -11,15 +11,32 @@ namespace UI
     public sealed class PR_UnitPlacementSelection : IDisposable
     {
         private readonly CompositeDisposable _disposable = new CompositeDisposable();
+        private readonly MDL_UnitPlacementField _mdl;
 
         public PR_UnitPlacementSelection(MDL_UnitPlacementField mdl, VW_UnitPlacementSelection view)
         {
             AssertHelper.NotNull(typeof(PR_UnitPlacementSelection), mdl);
             AssertHelper.NotNull(typeof(PR_UnitPlacementSelection), view);
-            
+
+            _mdl = mdl;
             mdl.SelectedNode
                 .Subscribe(view.ShowUnitPlacementField)
                 .AddTo(_disposable);
+            
+            view.unitSellBtn.OnClickAsObservable()
+                .Subscribe(SellUnit)
+                .AddTo(_disposable);
+        }
+
+        /// <summary>
+        /// 해당 노드의 유닛을 판매합니다.
+        /// </summary>
+        private void SellUnit(UniRx.Unit _)
+        {
+            UnitPlacementNode selectedNode = _mdl.GetSelectedNode();
+            AssertHelper.NotNull(typeof(PR_UnitPlacementSelection), selectedNode);
+            
+            selectedNode.SellUnit();
         }
         
         public void Dispose()
