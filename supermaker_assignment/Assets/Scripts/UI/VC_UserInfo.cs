3ad6161a00b0387442faa5799d3c967fa237546d
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-06 20:05:40 +0900

    feat: VC_UserInfo 클래스 구현

---
+++ supermaker_assignment/Assets/Scripts/UI/VC_UserInfo.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace UI
+{
+    /// <summary>
+    /// UserInfo와 관련된 Presenter와 View를 관리하는 클래스입니다.
+    /// </summary>
+    public class VC_UserInfo : ViewController
+    {
+        protected override void ValidateReferences()
+        {
+        }
+
+        public override void Init(DataManager dataManager)
+        {
+        }
+
+        protected override void ReleasePresenter()
+        {
+        }
+    }
+}
