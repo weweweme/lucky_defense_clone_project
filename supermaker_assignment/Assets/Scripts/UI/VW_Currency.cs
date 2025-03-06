6cb08fb6e34086a2015f3c189cbcb03b593e97f5
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-06 18:45:38 +0900

    feat: VW_Currency 내부 기능 구현

--- supermaker_assignment/Assets/Scripts/UI/VW_Currency.cs
+++ supermaker_assignment/Assets/Scripts/UI/VW_Currency.cs
@@ -1,4 +1,7 @@
 using InGame.System;
+using TMPro;
+using UnityEngine;
+using Util;
 
 namespace UI
 {
@@ -7,5 +10,20 @@ namespace UI
     /// </summary>
     public sealed class VW_Currency : View
     {
+        [SerializeField] private TextMeshProUGUI _currency;
+
+        private void Awake()
+        {
+            AssertHelper.NotNull(typeof(VW_Currency), _currency);
+        }
+
+        /// <summary>
+        /// 재화를 업데이트합니다.
+        /// </summary>
+        /// <param name="currency"></param>
+        private void UpdateCurrency(uint currency)
+        {
+            _currency.SetText(currency.ToString());
+        }
     }
 }
