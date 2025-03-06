6d31dc4c64afa3db073198df8dc8353cba968a2e
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-06 18:53:12 +0900

    feat: VC_UnitSpawn에서 Currency 관련 모듈을 초기화하는 코드 추가

--- supermaker_assignment/Assets/Scripts/UI/VC_UnitSpawn.cs
+++ supermaker_assignment/Assets/Scripts/UI/VC_UnitSpawn.cs
@@ -12,19 +12,25 @@ namespace UI
         [SerializeField] private VW_UnitSpawn _vwUnitSpawn;
         private readonly PR_UnitSpawn _prUnitSpawn = new PR_UnitSpawn();
         
+        [SerializeField] private VW_Currency _vwCurrency;
+        private readonly PR_Currency _prCurrency = new PR_Currency();
+        
         protected override void ValidateReferences()
         {
             AssertHelper.NotNull(typeof(VC_UnitSpawn), _vwUnitSpawn);
+            AssertHelper.NotNull(typeof(VC_UnitSpawn), _vwCurrency);
         }
 
         public override void Init(DataManager dataManager)
         {
             _prUnitSpawn.Init(dataManager, _vwUnitSpawn);
+            _prCurrency.Init(dataManager, _vwCurrency);
         }
 
         protected override void ReleasePresenter()
         {
             _prUnitSpawn.Dispose();
+            _prCurrency.Dispose();
         }
     }
 }
