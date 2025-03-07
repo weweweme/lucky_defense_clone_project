145edcdd5a805a2b111f900955d6fad1b9d55bbe
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-07 22:08:23 +0900

    feat: PR_MythicUnitCombinationButton 구현, Rx 연동

---
+++ supermaker_assignment/Assets/Scripts/UI/PR_MythicUnitCombinationButton.cs
@@ -0,0 +1,28 @@
+using System;
+using InGame.System;
+using Model;
+using UniRx;
+using Util;
+
+namespace UI
+{
+    /// <summary>
+    /// 신화 조합 패널을 여는 버튼의 Presenter 클래스입니다.
+    /// </summary>
+    public sealed class PR_MythicUnitCombinationButton : Presenter
+    {
+        public override void Init(DataManager dataManager, View view)
+        {
+            AssertHelper.NotNull(typeof(PR_MythicUnitCombinationButton), dataManager);
+            
+            VW_MythicUnitCombinationButton vw = view as VW_MythicUnitCombinationButton;
+            AssertHelper.NotNull(typeof(PR_MythicUnitCombinationButton), vw);
+
+            MDL_GameSystem mdl = dataManager.GameSystem;
+            vw!.openPanelBut.OnClickAsObservable()
+                .Where(_ => mdl.MythicCombinationPanelVisible.Value == false)
+                .Subscribe(_ => mdl.SetMythicCombinationPanelVisible(true))
+                .AddTo(disposable);
+        }
+    }
+}
