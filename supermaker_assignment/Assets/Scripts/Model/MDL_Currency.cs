8054e47878d77d25d769343a841156cabb1de1f6
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-08 02:37:50 +0900

    feat: MDL_Currency에 다이아 관련 Rx 추가

--- supermaker_assignment/Assets/Scripts/Model/MDL_Currency.cs
+++ supermaker_assignment/Assets/Scripts/Model/MDL_Currency.cs
@@ -7,12 +7,20 @@ namespace Model
     /// </summary>
     public class MDL_Currency
     {
+        // 골드 관련 Rx
         private const uint INITIAL_GOLD = 100;
         private readonly ReactiveProperty<uint> _gold = new ReactiveProperty<uint>(INITIAL_GOLD);
         public IReadOnlyReactiveProperty<uint> Gold => _gold;
-
         public uint GetGold() => _gold.Value;
         public void AddGold(uint amount) => _gold.Value += amount;
         public void SubtractGold(uint amount) => _gold.Value -= amount;
+
+        // 다이아몬드 관련 Rx
+        private const uint INITIAL_DIAMOND = 3;
+        private readonly ReactiveProperty<uint> _diamond = new ReactiveProperty<uint>(INITIAL_DIAMOND);
+        public IReadOnlyReactiveProperty<uint> Diamond => _diamond;
+        public uint GetDiamond() => _diamond.Value;
+        public void AddDiamond(uint amount) => _diamond.Value += amount;
+        public void SubtractDiamond(uint amount) => _diamond.Value -= amount;
     }
 }
