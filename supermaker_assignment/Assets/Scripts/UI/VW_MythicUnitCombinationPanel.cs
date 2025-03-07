a74a5712ecd98d26ca29e4baedfab4444c92024e
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-07 22:31:21 +0900

    feat: Visible을 off로 하는 기능 추가

--- supermaker_assignment/Assets/Scripts/UI/VW_MythicUnitCombinationPanel.cs
+++ supermaker_assignment/Assets/Scripts/UI/VW_MythicUnitCombinationPanel.cs
@@ -1,5 +1,6 @@
 using InGame.System;
 using UnityEngine;
+using UnityEngine.UI;
 using Util;
 
 namespace UI
@@ -10,10 +11,14 @@ namespace UI
     public sealed class VW_MythicUnitCombinationPanel : View
     {
         [SerializeField] internal Canvas canvas;
+        [SerializeField] internal Button exitBackgroundPanel;
+        [SerializeField] internal Button exitButton;
 
         private void Awake()
         {
             AssertHelper.NotNull(typeof(VW_MythicUnitCombinationPanel), canvas);
+            AssertHelper.NotNull(typeof(VW_MythicUnitCombinationPanel), exitBackgroundPanel);
+            AssertHelper.NotNull(typeof(VW_MythicUnitCombinationPanel), exitButton);
         }
         
         public void SetCanvasActive(bool isActive) => canvas.enabled = isActive;
