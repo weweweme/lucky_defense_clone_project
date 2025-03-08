f9ac2defbc7c48b9ba120c19bbcca197c03dc86e
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-08 15:12:56 +0900

    feat: VC_WaveInfo 직렬화 데이터 셋업

--- supermaker_assignment/Assets/Scripts/UI/VC_WaveInfo.cs
+++ supermaker_assignment/Assets/Scripts/UI/VC_WaveInfo.cs
@@ -15,22 +15,28 @@ namespace UI
         [SerializeField] private VW_CurrentWaveCount _vwCurrentWaveCount;
         private readonly PR_CurrentWaveCount _prCurrentWaveCount = new PR_CurrentWaveCount();
         
+        [SerializeField] private VW_CurrentRemainigEnemyCount _vwCurrentRemainigEnemyCount;
+        private readonly PR_CurrentRemainigEnemyCount _prCurrentRemainigEnemyCount = new PR_CurrentRemainigEnemyCount();
+        
         protected override void ValidateReferences()
         {
             AssertHelper.NotNull(typeof(VC_WaveInfo), _vwNextWaveTimer);
             AssertHelper.NotNull(typeof(VC_WaveInfo), _vwCurrentWaveCount);
+            AssertHelper.NotNull(typeof(VC_WaveInfo), _vwCurrentRemainigEnemyCount);
         }
 
         public override void Init(DataManager dataManager)
         {
             _prNextWaveTimer.Init(dataManager, _vwNextWaveTimer);
             _prCurrentWaveCount.Init(dataManager, _vwCurrentWaveCount);
+            _prCurrentRemainigEnemyCount.Init(dataManager, _vwCurrentRemainigEnemyCount);
         }
 
         protected override void ReleasePresenter()
         {
             _prNextWaveTimer.Dispose();
             _prCurrentWaveCount.Dispose();
+            _prCurrentRemainigEnemyCount.Dispose();
         }
     }
 }
