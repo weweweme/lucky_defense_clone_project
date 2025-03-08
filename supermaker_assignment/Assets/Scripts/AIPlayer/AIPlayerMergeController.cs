9b381808d85560899bc3180d291e5792d6bbcb79
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-08 17:01:57 +0900

    feat: AIPlayerMergeController 클래스 스크립트 추가

---
+++ supermaker_assignment/Assets/Scripts/AIPlayer/AIPlayerMergeController.cs
@@ -0,0 +1,11 @@
+using Util;
+
+namespace AIPlayer
+{
+    /// <summary>
+    /// AI 플레이어가 조합을 할 수 있는지 확인하고 수행하는 컨트롤러 클래스입니다.
+    /// </summary>
+    public sealed class AIPlayerMergeController : MonoBehaviourBase
+    {
+    }
+}
