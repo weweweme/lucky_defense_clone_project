b9cf9aa14ae76954ee2733f4fc05206f86b86304
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-08 21:58:38 +0900

    feat: AIUnitCombinationChecker 내포 구조체 구현

--- supermaker_assignment/Assets/Scripts/AIPlayer/AIPlayerMythicUnitCombinationController.cs
+++ supermaker_assignment/Assets/Scripts/AIPlayer/AIPlayerMythicUnitCombinationController.cs
@@ -1,16 +1,62 @@
+using System;
 using CleverCrow.Fluid.BTs.Tasks;
+using System.Collections.Generic;
+using Model;
 using Util;
 
 namespace AIPlayer
 {
     /// <summary>
-    /// AI Player의 신화 유닛 조합을 제어하는 클래스입니다.
+    /// AI 플레이어가 신화 유닛을 조합할 수 있는지 확인하고 수행하는 컨트롤러 클래스입니다.
     /// </summary>
     public sealed class AIPlayerMythicUnitCombinationController : MonoBehaviourBase
     {
+        /// <summary>
+        /// AI 플레이어의 유닛이 배치된 노드 목록입니다.
+        /// </summary>
+        private UnitPlacementNode[] _northGridNodes;
+
+        /// <summary>
+        /// 조합을 위한 유닛 조건 리스트입니다.
+        /// </summary>
+        private readonly List<AIUnitCombinationChecker> _combinationCheckers = new();
+
+        /// <summary>
+        /// AI가 신화 유닛 조합을 수행하기 위해 필요한 유닛 조건을 정의합니다.
+        /// </summary>
+        private struct AIUnitCombinationChecker
+        {
+            public readonly EUnitType ResultUnitType;
+            public readonly SUnitCombinationFlagCondition[] Conditions;
+
+            public AIUnitCombinationChecker(EUnitType resultType, params SUnitCombinationFlagCondition[] conditions)
+            {
+                ResultUnitType = resultType;
+                Conditions = conditions;
+            }
+        }
+        
+        private MDL_Unit _globalMdlUnit;
+
         public void Init(AIPlayerRoot root)
         {
-            
+            _northGridNodes = root.globalRootManager.UnitGridNodeManager.NorthGridNodes;
+            _globalMdlUnit = root.globalRootManager.DataManager.Unit;
+
+            // AI 전용 신화 조합 조건 정의
+            _combinationCheckers.Add(new AIUnitCombinationChecker(
+                EUnitType.Melee,
+                new SUnitCombinationFlagCondition(EUnitGrade.Common, EUnitType.Melee),
+                new SUnitCombinationFlagCondition(EUnitGrade.Rare, EUnitType.Melee),
+                new SUnitCombinationFlagCondition(EUnitGrade.Heroic, EUnitType.Melee)
+            ));
+
+            _combinationCheckers.Add(new AIUnitCombinationChecker(
+                EUnitType.Ranged,
+                new SUnitCombinationFlagCondition(EUnitGrade.Common, EUnitType.Ranged),
+                new SUnitCombinationFlagCondition(EUnitGrade.Rare, EUnitType.Ranged),
+                new SUnitCombinationFlagCondition(EUnitGrade.Heroic, EUnitType.Ranged)
+            ));
         }
 
         public bool CanCombination()
