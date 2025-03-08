02e16ebe4f41823b6fc9ae9edc9dc3e3c8930ab5
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-08 19:25:52 +0900

    feat: AIPlayerGambleController 클래스 스크립트 추가

---
+++ supermaker_assignment/Assets/Scripts/AIPlayer/AIPlayerGambleController.cs
@@ -0,0 +1,15 @@
+using Util;
+
+namespace AIPlayer
+{
+    /// <summary>
+    /// AI Player의 도박과 관련된 로직을 관리하는 컨트롤러 클래스입니다.
+    /// </summary>
+    public sealed class AIPlayerGambleController : MonoBehaviourBase
+    {
+        public void Init(AIPlayerRoot root)
+        {
+            
+        }
+    }
+}
