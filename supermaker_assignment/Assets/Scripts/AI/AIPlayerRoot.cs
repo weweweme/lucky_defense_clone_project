50ee8bd4d34fdd1d7cfa7983790b0a890ee1c185
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-08 16:40:36 +0900

    feat: AIPlayerBTController 클래스 셋업

--- supermaker_assignment/Assets/Scripts/AI/AIPlayerRoot.cs
+++ supermaker_assignment/Assets/Scripts/AI/AIPlayerRoot.cs
@@ -5,7 +5,7 @@ namespace AI
     /// <summary>
     /// AI의 루트 클래스입니다.
     /// </summary>
-    public sealed class AIRoot : MonoBehaviourBase
+    public sealed class AIPlayerRoot : MonoBehaviourBase
     {
     }
 }
