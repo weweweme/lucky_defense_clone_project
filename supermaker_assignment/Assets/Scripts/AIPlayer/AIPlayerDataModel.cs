2939207406b3407498c64b6cd75fb524b71d0b5f
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-08 17:37:53 +0900

    feat: AIPlayer의 골드 관련 데이터 멤버 추가

--- supermaker_assignment/Assets/Scripts/AIPlayer/AIPlayerDataModel.cs
+++ supermaker_assignment/Assets/Scripts/AIPlayer/AIPlayerDataModel.cs
@@ -5,5 +5,11 @@ namespace AIPlayer
     /// </summary>
     public class AIPlayerDataModel
     {
+        // 골드 관련 데이터
+        private const uint INITIAL_GOLD = 100;
+        private uint _gold = INITIAL_GOLD;
+        public uint GetGold() => _gold;
+        public void AddGold(uint amount) => _gold += amount;
+        public void SubtractGold(uint amount) => _gold -= amount;
     }
 }
