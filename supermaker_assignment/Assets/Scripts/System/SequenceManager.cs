c0d6659763e66e26f0b7cd04e13baa624379806e
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-08 16:47:55 +0900

    feat: SequenceManager 스크립트 추가

---
+++ supermaker_assignment/Assets/Scripts/System/SequenceManager.cs
@@ -0,0 +1,12 @@
+using Util;
+
+namespace System
+{
+    /// <summary>
+    /// 게임의 시퀀스를 관리하는 매니저 클래스입니다.
+    /// </summary>
+    public sealed class SequenceManager : MonoBehaviourBase
+    {
+        
+    }
+}
