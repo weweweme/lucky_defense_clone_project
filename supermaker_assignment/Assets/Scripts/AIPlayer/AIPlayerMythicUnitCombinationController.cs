89895a0cc7f389eabb74fbce7c0a8b147f551f6e
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-08 21:40:01 +0900

    feat: AIPlayerMythicUnitCombinationController 클래스 스크립트 셋업

---
+++ supermaker_assignment/Assets/Scripts/AIPlayer/AIPlayerMythicUnitCombinationController.cs
@@ -0,0 +1,15 @@
+using Util;
+
+namespace AIPlayer
+{
+    /// <summary>
+    /// AI Player의 신화 유닛 조합을 제어하는 클래스입니다.
+    /// </summary>
+    public sealed class AIPlayerMythicUnitCombinationController : MonoBehaviourBase
+    {
+        public void Init(AIPlayerRoot root)
+        {
+            
+        }
+    }
+}
