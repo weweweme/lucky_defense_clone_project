5424e27cc0bf2a3fad4115c2aef83c11b3c77767
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-09 16:28:01 +0900

    feat: 참조 세팅

--- supermaker_assignment/Assets/Scripts/UI/UnitSkillPoolManager.cs
+++ supermaker_assignment/Assets/Scripts/UI/UnitSkillPoolManager.cs
@@ -22,7 +22,7 @@ namespace UI
             AssertHelper.NotNull(typeof(UnitSkillPoolManager), _rareRangeSkillPool);
             AssertHelper.NotNull(typeof(UnitSkillPoolManager), _rareMeleeSkillPool);
             AssertHelper.NotNull(typeof(UnitSkillPoolManager), _heroRangeSkillPool);
-            // AssertHelper.NotNull(typeof(UnitSkillPoolManager), _heroMeleeSkillPool);
+            AssertHelper.NotNull(typeof(UnitSkillPoolManager), _heroMeleeSkillPool);
         }
 
         /// <summary>
@@ -38,7 +38,7 @@ namespace UI
                 (EUnitGrade.Rare, EUnitType.Ranged) => _rareRangeSkillPool.GetObject(),
                 (EUnitGrade.Rare, EUnitType.Melee) => _rareMeleeSkillPool.GetObject(),
                 (EUnitGrade.Hero, EUnitType.Ranged) => _heroRangeSkillPool.GetObject(),
-                // (EUnitGrade.Hero, EUnitType.Melee) => _heroMeleeSkillPool.GetObject(),
+                (EUnitGrade.Hero, EUnitType.Melee) => _heroMeleeSkillPool.GetObject(),
                 // (EUnitGrade.Mythic, EUnitType.Melee) => _mythicMeleeSkillPool.GetObject(),
                 // (EUnitGrade.Mythic, EUnitType.Ranged) => _mythicRangeSkillPool.GetObject(),
                 _ => null // 신화 등급 스킬은 별도 관리
