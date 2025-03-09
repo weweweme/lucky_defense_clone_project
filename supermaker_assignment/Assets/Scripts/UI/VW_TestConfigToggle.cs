99fd98f86be43cd1ab4dbdcb0467b9eb3f30f6bf
JinSoo Joo Mac <argentdarae@gmail.com>
2025-03-09 10:54:31 +0900

    feat: VW_TestConfigToggle 클래스 구현

---
+++ supermaker_assignment/Assets/Scripts/UI/VW_TestConfigToggle.cs
@@ -0,0 +1,36 @@
+using System;
+using InGame.System;
+using UnityEngine;
+using UnityEngine.UI;
+using Util;
+
+namespace UI
+{
+    /// <summary>
+    /// 테스트 설정 패널을 열고 닫는 토글의 View 클래스입니다.
+    /// </summary>
+    public sealed class VW_TestConfigToggle : View
+    {
+        [SerializeField] internal Button testConfigToggle;
+        [SerializeField] private RectTransform _directionArrow;
+        
+        private void Awake()
+        {
+            AssertHelper.NotNull(typeof(VW_TestConfigToggle), testConfigToggle);
+            AssertHelper.NotNull(typeof(VW_TestConfigToggle), _directionArrow);
+        }
+
+        public void SetArrowDirection(ETestConfigState state)
+        {
+            AssertHelper.NotEqualsEnum(typeof(VW_TestConfigToggle), state, ETestConfigState.None);
+            
+            switch (state)
+            {
+                case ETestConfigState.Open:
+                    break;
+                case ETestConfigState.Close:
+                    break;
+            }
+        }
+    }
+}
