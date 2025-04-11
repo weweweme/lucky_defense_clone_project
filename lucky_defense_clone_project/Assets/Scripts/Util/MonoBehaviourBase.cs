572b9720abc8e74fccf270f425049dfec2bfff14
Jinsoo <argentdarae@gmail.com>
2025-04-11 23:24:11 +0900

    chore: MonoBehaviourBase 수정

--- lucky_defense_clone_project/Assets/Scripts/Util/MonoBehaviourBase.cs
+++ lucky_defense_clone_project/Assets/Scripts/Util/MonoBehaviourBase.cs
@@ -1,5 +1,5 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using UnityEngine;
@@ -11,7 +11,7 @@ namespace Util
      * - Unity에서 MonoBehaviour 객체는 C++ 객체(gameObject)와 C# 객체(MonoBehaviour)로 나뉘어 관리되며,
      *   C# 객체는 가비지 컬렉션(GC)에 의해 수명이 관리된다.
      * - MonoBehaviour 객체가 파괴되더라도 해당 객체가 참조하고 있는 모든 레퍼런스가 해제되지 않으면
-     *   GC가 이를 제거하지 못하고 메모리 상에 남아 있게 된다. 이를 "Ghost Reference" 문제라고 한다.
+     *   GC가 이를 제거하지 못하고 메모리 상에 남아 있게 된다. 이를 "Dangling Reference" 문제라고 한다.
      * - 이러한 문제는 레퍼런스 그래프의 비대화와 게임 퍼포먼스 저하(틱 불안정)를 초래할 수 있다.
      * - 이를 해결하기 위해 OnDestroy에서 모든 멤버 변수의 참조를 명시적으로 null로 설정하거나 컬렉션을 비우는 작업을 수행해야 한다.
      *
@@ -35,37 +35,32 @@ namespace Util
 
             foreach (FieldInfo field in fields)
             {
-                // 값 타입(Primitive Type)은 처리할 필요 없음 → continue
-                if (field.FieldType.IsValueType) continue;
-                
+                if (field.FieldType.IsValueType || field.IsInitOnly)
+                    continue;
+
                 var fieldValue = field.GetValue(this);
-                switch (fieldValue)
+                if (fieldValue == null)
+                    continue;
+
+                // 기본 컬렉션 인터페이스로 GC 대상 데이터만 Clear 처리
+                if (fieldValue is IList list)
                 {
-                    case null:
-                        continue;
-                    
-                    case IList list:
-                        list.Clear();
-                        break;
-                    
-                    case IDictionary dictionary:
-                        dictionary.Clear();
-                        break;
-                    
-                    case Queue queue:
-                        queue.Clear();
-                        break;
-                    
-                    case Stack stack:
-                        stack.Clear();
-                        break;
-                    
-                    case HashSet<object> hashSet:
-                        hashSet.Clear();
-                        break;
+                    list.Clear();
+                }
+                else if (fieldValue is IDictionary dictionary)
+                {
+                    dictionary.Clear();
+                }
+                else
+                {
+                    // 그 외: HashSet<T>, LinkedList<T> 등 Clear() 메서드가 있는 컬렉션
+                    var clearMethod = fieldValue.GetType().GetMethod("Clear", Type.EmptyTypes);
+                    if (clearMethod != null && clearMethod.DeclaringType != typeof(string)) // 문자열은 Clear 없음, 방어
+                    {
+                        clearMethod.Invoke(fieldValue, null);
+                    }
                 }
 
-                // 참조 해제하여 GC가 즉시 정리할 수 있도록 함
                 field.SetValue(this, null);
             }
         }
